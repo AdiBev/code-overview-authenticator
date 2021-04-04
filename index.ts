6c8bc9e5918728727e389261e9775bbefe7176ef
Adi <aditaybev@gmail.com>
2021-04-04 21:40:00 +0100

    move index.ts file to root folder

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import router from "./routes/authenticate";
+import router from "./src/routes/authenticate";
 
 const app = express();
 const PORT = 8000;
 app.use("/api", router);
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.get("/", (_, res) => res.send("<b>Code overview authenticator</b>"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
