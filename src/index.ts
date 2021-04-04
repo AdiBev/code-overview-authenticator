ad9decdfa56da9da1e0f856b8b73bdd02cac69a5
Adi <aditaybev@gmail.com>
2021-04-04 22:00:22 +0100

    add header for testing

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import router from "./routes/authenticate";
 
 const app = express();
 const PORT = 8000;
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  next();
+});
 app.use("/api", router);
 app.get("/", (_, res) => res.send("<b>Code overview authenticator</b>"));
 app.listen(PORT, () => {
