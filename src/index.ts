ce174f67705d16774178fcf4418e9a328872c43f
AdiBev <aditaybev@gmail.com>
2021-04-05 21:10:58 +0100

    format url

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,7 @@ const app = express();
 const PORT = 8000;
 //allow access to my own doamin only
 app.use((_, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://code-overview.vercel.app/"
-  );
+  res.header("Access-Control-Allow-Origin", "https://code-overview.vercel.app");
   next();
 });
 app.use("/api", router);
