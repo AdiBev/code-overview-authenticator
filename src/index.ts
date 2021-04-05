fb8465df797bbbd6bba5168ceedf709d5af3929c
AdiBev <aditaybev@gmail.com>
2021-04-05 21:03:38 +0100

    update access allowed url

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import router from "./routes/authenticate";
 
 const app = express();
 const PORT = 8000;
+//allow access to my own doamin only
 app.use((_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://code-overview.vercel.app/"
+  );
   next();
 });
 app.use("/api", router);
